7f84333b1716488b247bb7bbdab56debb1fb2df8
Kevin David L. Girs <69004224+kevindavidlgirs@users.noreply.github.com>
2022-06-22 12:57:08 +0200

    Update ia.py

--- ia.py
+++ ia.py
@@ -13,7 +13,7 @@ class IA:
     def __init_neurons(self,):
         [self.__neurons.append([0 for i in range(0, self.__layers[idx])]) for idx in range(0, len(self.__layers))]
     
-    def __feed_fordward(self,inputs):
+    def __propagation(self,inputs):
         self.__neurons[0] = inputs
         for idx in range(1,len(self.__neurons)):
             self.__neurons[idx] = tools.sigmoid(np.dot(self.__neurons[idx-1], self.__weights[idx-1]))
@@ -23,7 +23,7 @@ class IA:
         delta = []        
         delta.append(error*tools.sigmoidPrime(self.__neurons[len(self.__neurons)-1]))
         for i in reversed(range(0, len(self.__neurons)-1)):
-            hidden_layer_error = np.dot(delta[len(self.__neurons)-2-i], self.__weights[i].T)
-            delta.append(hidden_layer_error*tools.sigmoidPrime(self.__neurons[i]))
+            h_l_error = np.dot(delta[len(self.__neurons)-2-i], self.__weights[i].T)
+            delta.append(h_l_error*tools.sigmoidPrime(self.__neurons[i]))
         for i in reversed(range(0, len(self.__weights))):
             self.__weights[i] += np.dot(self.__neurons[i].T, delta[len(self.__weights)-1-i])
