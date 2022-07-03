90fe30b26cd4b80a554ce731f7058979321c6bfd
Kevin David L. Girs <kevindavidgirs@protonmail.com>
2022-07-03 21:05:18 +0200

    Rectification

--- ia.py
+++ ia.py
@@ -1,29 +1,33 @@
 class IA:
     
-    def __init__(self,layers:list) -> None:
+    def __init__(self,name:str,layers:list):
         self.__layers = layers
         self.__neurons = []
         self.__weights = []
-        self.__init_neurons()
+        self.__init_network()
+
+    def __init_network(self,) -> bool:
+        self.__init_neurons():
         self.__init_weights()
 
-    def __init_weights(self,):
+    def __init_neurons(self,) -> bool:
+        [self.__neurons.append(np.array([0 for i in range(0, self.__layers[idx])])) for idx in range(0, len(self.__layers))]
+
+    def __init_weights(self,) -> bool:
         [[self.__weights.append(2*np.random.random((len(self.__neurons[idx]), len(self.__neurons[idx+1])))-1)] for idx in range(0, len(self.__neurons)-1)]
 
-    def __init_neurons(self,):
-        [self.__neurons.append([0 for i in range(0, self.__layers[idx])]) for idx in range(0, len(self.__layers))]
-    
-    def __propagation(self,inputs):
+    def propagation(self,inputs) -> bool:
         self.__neurons[0] = inputs
         for idx in range(1,len(self.__neurons)):
             self.__neurons[idx] = tools.sigmoid(np.dot(self.__neurons[idx-1], self.__weights[idx-1]))
-    
-    def __back_propagation(self,responses):
-        error = (responses - self.__neurons[len(self.__neurons)-1])
+
+    def back_propagation(self,responses) -> bool:
+        error = (responses - np.array(self.__neurons[len(self.__neurons)-1]))
         delta = []        
         delta.append(error*tools.sigmoidPrime(self.__neurons[len(self.__neurons)-1]))
-        for i in reversed(range(0, len(self.__neurons)-1)):
-            h_l_error = np.dot(delta[len(self.__neurons)-2-i], self.__weights[i].T)
-            delta.append(h_l_error*tools.sigmoidPrime(self.__neurons[i]))
+        self.__price =  str(np.mean(np.abs(error)))
+        for i in reversed(range(1, len(self.__weights))):
+            error = np.dot(delta[len(self.__weights)-1-i], self.__weights[i].T)
+            delta.append(error*tools.sigmoidPrime(self.__neurons[i]))
         for i in reversed(range(0, len(self.__weights))):
             self.__weights[i] += np.dot(self.__neurons[i].T, delta[len(self.__weights)-1-i])
